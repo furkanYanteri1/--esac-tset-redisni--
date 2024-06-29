97b298334babb2c50a0ba963d84d1884dbdac6b3
Furkan Yanteri <furkanyanteri@gmail.com>
2024-06-29 10:34:37 +0300

    feat: divide home view into 4 components

---
+++ src/components/LeftComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="left-component">Left Component</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "LeftComponent",
+});
+</script>
+
+<style scoped>
+.left-component {
+  background-color: #e74c3c;
+  padding: 10px;
+  text-align: center;
+  height: 100%;
+}
+</style>
